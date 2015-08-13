1c73f988305a672eb563545f2f8f02d16bcc55be
89berner <89berner@gmail.com>
2015-08-13 19:05:26 -0300

    Update create.sh

--- create.sh
+++ create.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-/usr/bin/aws dynamodb put-item --table-name credentials --item "{ \"name\": {\"S\": \"$1\"}, \"value\": {\"S\": \"$2\"}  }"  --return-consumed-capacity TOTAL
+KEYID=$3
+
+VALUE=$(aws kms encrypt --key-id $KEYID --plaintext "$2" --query CiphertextBlob --output text)
+
+/usr/bin/aws dynamodb put-item --table-name credentials --item "{ \"name\": {\"S\": \"$1\"}, \"value\": {\"S\": \"$VALUE\"}  }"  --return-consumed-capacity TOTAL
