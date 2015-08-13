3006ba3d0f114247ed26cf5d8751b2c09be70953
89berner <89berner@gmail.com>
2015-08-13 19:21:42 -0300

    Update get.sh

--- get.sh
+++ get.sh
@@ -2,10 +2,14 @@
 
 user=$(echo $1 | tr -dc '[:alnum:]_\n\r' | tr '[:upper:]' '[:lower:]')
 
-RESPONSE=$(aws dynamodb get-item --table-name credentials --key "{\"name\":{\"S\":\"$user\"}}" --region us-west-2 --attributes-to-get value 2>/dev/null|grep "S"|cut -d ":" -f 2|cut -d '"' -f 2)
+RESPONSE=$(aws dynamodb get-item --table-name credentials --key "{\"name\":{\"S\":\"$user\"}}" --region us-east-1 --attributes-to-get value 2>/dev/null|grep "S"|cut -d ":" -f 2|cut -d '"' -f 2)
+
+echo "$RESPONSE" | base64 --decode > /tmp/encrypted
+
+DRESPONSE=$(aws kms decrypt --ciphertext-blob fileb:///tmp/encrypted  --output text --query Plaintext | base64 --decode)
 
 if [ -z "$RESPONSE" ]; then
 	echo -n "Error"
 else
-	echo -n $RESPONSE
+	echo -n $DRESPONSE
 fi
